352507b8d46af7aa531a456240e4b29e7bade65a
surya06 <surya.thurangi@gmail.com>
2024-10-16 07:59:18 +0530

    first commit

---
+++ src/webparts/spFxCicd/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ISpFxCicdWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'SpFxCicdWebPartStrings' {
+  const strings: ISpFxCicdWebPartStrings;
+  export = strings;
+}
